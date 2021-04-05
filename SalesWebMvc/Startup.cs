d95dda77773df35ba642b360864a98e21a258daa
Luis Molero <luis.molero@outlook.com>
2021-04-05 03:56:35 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services;
 using SalesWebMvc.Data;
 
 namespace SalesWebMvc
@@ -34,6 +35,7 @@ namespace SalesWebMvc
             });
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
